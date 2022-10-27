2f0d46107662a770339415d3a64923a375a76d95
gemy <mohamed.gamal.mg136@gmail.com>
2022-10-27 10:27:15 +0200

    updating register page

--- src/pages/register/Form.jsx
+++ src/pages/register/Form.jsx
@@ -157,7 +157,14 @@ const RegisterForm = () => {
         </Form.Item>
 
         <Form.Item>
-          <PrimaryBtn title={"SignUp"} htmlType="submit" />
+          <button
+            className="primaryBtn border-0 text-uppercase 
+                     fw-bold shadow shadow rounded-pill
+                    px-5 py-2 text-light"
+            htmlType="submit"
+          >
+            Sign Up
+          </button>
         </Form.Item>
       </div>
     </Form>
