8b924e98b0ee44d55ebe1a8276742d452692a794
gemy <mohamed.gamal.mg136@gmail.com>
2022-11-01 06:57:38 +0200

    home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,17 @@
+import Slider from "../../components/carousel";
+import Category from "./Category";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="row pt-5">
+      <div className="col-md-3"></div>
+      <div className="col-md-6">
+        <Slider />
+        <Category />
+      </div>
+      <div className="col-md-3"></div>
+    </section>
+  );
 };
 
 export default Home;
