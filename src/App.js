28905289fca57438ae9664d81a4f15fd45edb170
osama7007 <saadosama43@gmail.com>
2022-11-03 17:12:11 +0200

    add top doctors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Welcome from "./pages/welcome";
 import SignUp from "./pages/signup";
 import Login from "./pages/login";
 import Patient from "./pages/patient";
-
+import TopRated from "./pages/top_rated";
 import Layout from "./layout";
 import useDoctors from "./hooks/useDoctors";
 
@@ -14,21 +14,20 @@ import ProfilePatient from "./pages/profilePatient";
 function App() {
   useDoctors();
   return (
-    <BrowserRouter>
-
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/patient" element={<Patient />} />
-          <Route path="/profilePatient" element={<ProfilePatient />} />
-        </Routes>
-      </Layout>
-
-    </BrowserRouter>
-  );
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<Welcome />} />
+					<Route path='/signup' element={<SignUp />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/home' element={<Home />} />
+					<Route path='/patient' element={<Patient />} />
+					<Route path='/profilePatient' element={<ProfilePatient />} />
+					<Route path='/topRated' element={<TopRated />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
 }
 
 export default App;
