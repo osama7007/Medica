a2de48f762e1e74dc1d3f0a89bd841c6dc3bc657
sandro <sandrosamy8@gmail.com>
2022-11-12 20:02:05 +0200

    all reminder except completed and canceled style

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -9,45 +9,41 @@ import { slugifyDoctor } from "../../utils/slugify";
 
 const Card = ({ img, title, rate, position, experince, specialty }) => {
   const navigate = useNavigate();
+
   const navigateappontment = () => {
-    navigate('/appointment');
+    navigate(`/appointment?doctor=${slugifyDoctor(title)}`);
   };
   
+
   return (
     <motion.div
-    layout
-    className={`${styles.cardContainer} d-flex align-items-center justify-content-between shadow rounded-4 mb-2 px-2 ps-3  mx-2`}
+      layout
+      className={`${styles.cardContainer} d-flex align-items-center justify-content-between shadow rounded-4 mb-2 px-2 ps-3  mx-2`}
     >
-      <div> 
-      <Link
-                to={`/doctors/${slugifyDoctor(title)}`}
-                className=" col-2 "
-                >
-        <div className={`${styles.imgContainer} mb-2`}>
-          <img src={img} alt={title} className=" rounded-4" />
-        </div>
-                </Link>
-                <Link
-                to={`/doctors/${slugifyDoctor(title)}`}
-                className=" col-2 text-decoration-none text-dark "
-                >
-        <p className="fw-bold fs-5 mb-1 text-center ">{title}</p>
-                </Link>
-                <Link
-                to={`/doctors/${slugifyDoctor(title)}`}
-                className=" col-2 text-decoration-none text-dark "
-                >
-        <p className="text-center text-uppercase text-blue bg-lightBlue p-2">
-          {specialty}
-        </p>
+      <div>
+        <Link to={`/doctors/${slugifyDoctor(title)}`} className=" col-2 ">
+          <div className={`${styles.imgContainer} mb-2`}>
+            <img src={img} alt={title} className=" rounded-4" />
+          </div>
         </Link>
-
-      </div> 
-
+        <Link
+          to={`/doctors/${slugifyDoctor(title)}`}
+          className=" col-2 text-decoration-none text-dark "
+        >
+          <p className="fw-bold fs-5 mb-1 text-center ">{title}</p>
+        </Link>
+        <Link
+          to={`/doctors/${slugifyDoctor(title)}`}
+          className=" col-2 text-decoration-none text-dark "
+        >
+          <p className="text-center text-uppercase text-blue bg-lightBlue p-2">
+            {specialty}
+          </p>
+        </Link>
+      </div>
 
       <div>
         <div className="d-flex flex-wrap align-items-center gap-4 text-center mb-4">
-
           <div>
             <BsFillFileEarmarkBarGraphFill className={styles.icons} />
             <p className="text-blue fw-bold mb-1">{experince}</p>
@@ -65,7 +61,6 @@ const Card = ({ img, title, rate, position, experince, specialty }) => {
             <p className="text-blue fw-bold mb-1">{position}</p>
             <small className="text-black-50">Position</small>
           </div>
-
         </div>
         <div className="text-center">
           <BlueBtn title="Book Appointment" action={navigateappontment} />
